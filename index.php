87a7490287d8cc82b2bc88c93792ccd0e80be785
Maite <martinezf.maite@gmail.com>
2025-05-11 19:26:58 -0300

    Add cover image

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
-<?<php>
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello, World! This is a simple PHP script.";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pachepé</title>
 
-</php>
+  <link rel="stylesheet" href="CSS/main.css" type="text/css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Ultra&display=swap" rel="stylesheet">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kirang+Haerang&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <main>
+    <div class="homeContainer">
+      <h2 class="title titleFirstPart">We serve Burgers</h2>
+      <h2 class="title titleSecondPart">WITH LOVE</h2>
+    </div>
+  </main>
+</body>
+</html>
