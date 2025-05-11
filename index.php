592137ce472d95773ec8c358035be687e6af2fc2
Maite <martinezf.maite@gmail.com>
2025-05-11 20:19:16 -0300

    Add navbar

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pachepé</title>
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
   <link rel="stylesheet" href="CSS/main.css" type="text/css">
+  <link rel="stylesheet" href="CSS/navbar.css" type="text/css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,11 +21,11 @@
 </head>
 
 <body>
-  <main>
-    <div class="homeContainer">
-      <h2 class="title titleFirstPart">We serve Burgers</h2>
-      <h2 class="title titleSecondPart">WITH LOVE</h2>
-    </div>
-  </main>
+  <div class="homeContainer">
+    <?php include 'components/navbar.php'; ?>
+
+    <h2 class="title titleFirstPart">We serve Burgers</h2>
+    <h2 class="title titleSecondPart">WITH LOVE</h2>
+  </div>
 </body>
 </html>
