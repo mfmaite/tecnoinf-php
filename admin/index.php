6cd3bbcff60c042bba540a07b802ffb05d360706
Maite <martinezf.maite@gmail.com>
2025-05-13 17:24:22 -0300

    Cambios en la organización de los CSS

--- admin/index.php
+++ admin/index.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pachepé</title>
+  <title>ADMIN</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="CSS/home.css" type="text/css">
+  <link rel="stylesheet" href="../CSS/home.css" type="text/css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -21,10 +21,9 @@
 
 <body>
   <div class="homeContainer">
-    <?php include 'components/navbar.php'; ?>
+    <?php include '../components/navbar-admin.php'; ?>
 
-    <h2 class="title titleFirstPart">We serve Burgers</h2>
-    <h2 class="title titleSecondPart">WITH LOVE</h2>
+    <h2 class="title titleSecondPart">Admin Panel</h2>
   </div>
 </body>
 </html>
--- components/navbar-admin.php
+++ components/navbar-admin.php
@@ -15,7 +15,7 @@
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbarComponent">
-  <a class="navbar-brand brandTitle" href="#">PACHEPÉ</a>
+  <a class="navbar-brand brandTitle" href="#">ADMIN</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
