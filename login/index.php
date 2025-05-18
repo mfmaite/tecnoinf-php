8c4b9e59d2c267a9c25a72f06063c07c21138feb
Maite <martinezf.maite@gmail.com>
2025-05-18 14:52:42 -0300

    Update login form styles

--- login/index.php
+++ login/index.php
@@ -34,20 +34,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>Iniciar sesión</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="../CSS/fonts.css">
+  <link rel="stylesheet" href="../CSS/home.css">
+  <link rel="stylesheet" href="../CSS/auth-pages.css">
 </head>
 <body>
   <?php include '../components/navbar.php'; ?>
 
-  <h1>Iniciar sesión</h1>
   <?php foreach ($errors as $error): ?>
     <p style="color: red;"><?= htmlspecialchars($error) ?></p>
   <?php endforeach; ?>
 
-  <form method="POST">
-    <label>Email: <input type="email" name="email" required></label><br>
-    <label>Contraseña: <input type="password" name="password" required></label><br>
-    <button type="submit">Entrar</button>
-  </form>
-  <p>¿No tenés cuenta? <a href="/restaurant/signup">Registrate</a></p>
+  <div class="homeContainer">
+    <div class="overlay"> </div>
+  </div>
+
+  <div class="loginContainer">
+    <h1 class="title-text text-center mb-4">Iniciar sesión</h1>
+    <form method="POST">
+      <div class="formFieldComponent">
+        <label class="formLabel" for="email">Email</label>
+        <input class="formInput" type="email" name="email" required>
+      </div>
+
+      <div class="formFieldComponent">
+        <label class="formLabel" for="password">Contraseña</label>
+        <input class="formInput" type="password" name="password" required>
+      </div>
+
+      <div class="form-group buttonContainer">
+        <button type="submit" class="btn btn-primary">Entrar</button>
+      </div>
+    </form>
+
+    <p class="signup-link text-center">¿No tenés cuenta? <a href="/restaurant/signup">Registrate</a></p>
+  </div>
 </body>
 </html>
